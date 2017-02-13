354cb79160953998020246577353785f8f7be0bf
Kapil Sharma <sharmakapil8@gmail.com>
2017-02-13 00:09:25 -0600

    First Cut
    Updated the website and the content

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,7 +1,18 @@
-$(document).ready(function () {
-    $('.carousel').carousel({
-        interval: 3000
-    });
+var image = new Array ();
+image[0] = "../assets/images/bp1.png";
+image[1] = "../assets/images/bp2.png";
+image[2] = "../assets/images/bp3.png";
+image[3] = "../assets/images/bp4.png";
+image[4] = "../assets/images/bp5.png";
+var size = image.length
 
-    $('.carousel').carousel('cycle');
-});
+var i = 0;
+setInterval(fadeDivs, 3000);
+
+function fadeDivs() {
+    $('.random-img img').fadeOut(100, function(){
+        var x = Math.floor(size*Math.random())
+        $(this).attr('src', image[x]).fadeIn(100);
+    })
+    i++;
+}
