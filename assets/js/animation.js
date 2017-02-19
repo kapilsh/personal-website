8330495affa0a955e4fbf8c55cd608ce07508a94
Kapil Sharma <sharmakapil8@gmail.com>
2017-02-19 00:31:27 -0600

    Changed Images for Archive

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -4,6 +4,8 @@ image[1] = "../assets/images/bp2.png";
 image[2] = "../assets/images/bp3.png";
 image[3] = "../assets/images/bp4.png";
 image[4] = "../assets/images/bp5.png";
+image[5] = "../assets/images/bp6.png";
+image[6] = "../assets/images/bp7.png";
 var size = image.length
 
 var i = 0;
