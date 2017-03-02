2cd941c39b8d38b66cfd6ccbe8cc2cf21bf16c3c
Kapil Sharma <sharmakapil8@gmail.com>
2017-03-02 07:34:01 -0600

    Fixed the javascript code

--- js/animation.js
+++ js/animation.js
@@ -1,10 +1,3 @@
-//
-// $(document).ready(function() {
-//     $(window).load(function() {
-      $('table.dataframe').addClass('table').addClass('table-striped').addClass('table-hover');
-    //   $(".input_prompt,.output_prompt").remove();
-      $('.bk-root').css({'height' : 400});
-    //   $('h1').addClass("page-header");
-      $('pre.highlight code').addClass('syntax')
-//     });
-// });
+$('table.dataframe').addClass('table').addClass('table-striped').addClass('table-hover');
+$('.bk-root').css({'height' : 400});
+$('pre.highlight code').addClass('syntax')
