4dc9c0aa5684c97653aff8786df0b4b592e64bcb
Kapil Sharma <sharmakapil8@gmail.com>
2018-09-09 23:00:05 -0500

    Fieed output formatting

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,3 @@
 $('.bk-root').css({'margin-bottom': '20px'});
 $('.language-python pre.highlight code').addClass('syntax')
-$('pre:has(.language-out)').addClass("output")
+$('pre:has(.language-out)').addClass("box")
