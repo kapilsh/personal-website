403e91f71ffbd82d7dd8772283296ce925814812
Kapil Sharma <kapilsharma.frm@gmail.com>
2020-07-11 16:07:48 -0500

    Removed the PUBLIC_URL

--- App.jsx
+++ App.jsx
@@ -22,11 +22,7 @@ class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <Router
-          onUpdate={() => window.scrollTo(0, 0)}
-          history={history}
-          basename={`${process.env.PUBLIC_URL}`}
-        >
+        <Router onUpdate={() => window.scrollTo(0, 0)} history={history}>
           <Layout />
         </Router>
       </Provider>
