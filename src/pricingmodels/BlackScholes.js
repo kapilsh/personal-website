68512543a74674ae2ad84c0ef41cc6261a23520e
Kapil Sharma <kapilsharma.frm@gmail.com>
2020-07-14 21:30:07 -0500

    Got pricing working

--- src/pricingmodels/BlackScholes.js
+++ src/pricingmodels/BlackScholes.js
@@ -1,9 +1,9 @@
-import { NormalDistribution } from "./Distributions";
+import { StandardNormalDistribution } from "./Distributions";
 
-export const BS = (s, f, vol, t, r) => {
+export const BS = (s, k, vol, t, r) => {
   const sigmaSqrtT = vol * Math.sqrt(t);
   const d1 = Math.log(s / k) / sigmaSqrtT + sigmaSqrtT / 2;
   const d2 = d1 - sigmaSqrtT;
   const pvk = k * Math.exp(-r * t);
-  return NormalDistribution.cdf(d1) * s - pvk * NormalDistribution.cdf(d2);
+  return StandardNormalDistribution.cdf(d1) * s - pvk * StandardNormalDistribution.cdf(d2);
 };
--- src/pricingmodels/Distributions.js
+++ src/pricingmodels/Distributions.js
@@ -1,10 +1,21 @@
-const mathjs = import("mathjs");
+import { erf, sqrt } from 'mathjs'
+
 
 const NormalDistribution = {
-  cdf: (x, mean, standardDeviation) => {
-    return (
-      (1 - mathjs.erf((mean - x) / (Math.sqrt(2) * standardDeviation))) / 2
-    );
+  cdf: (x, mean, sigma) => {
+    var z = (x - mean) / Math.sqrt(2 * sigma * sigma);
+    var t = 1 / (1 + 0.3275911 * Math.abs(z));
+    var a1 = 0.254829592;
+    var a2 = -0.284496736;
+    var a3 = 1.421413741;
+    var a4 = -1.453152027;
+    var a5 = 1.061405429;
+    var erf = 1 - (((((a5 * t + a4) * t) + a3) * t + a2) * t + a1) * t * Math.exp(-z * z);
+    var sign = 1;
+    if (z < 0) {
+      sign = -1;
+    }
+    return (1 / 2) * (1 + sign * erf);
   },
 };
 
