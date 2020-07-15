4a189cf8881fb094f2c5e8d5765eb4723d7a7605
Kapil Sharma <kapilsharma.frm@gmail.com>
2020-07-15 18:50:50 -0500

    Added   BS Pricer

--- src/components/options/PricingPanel.jsx
+++ src/components/options/PricingPanel.jsx
@@ -1,102 +1,28 @@
 import React from "react";
-import { PageHeader, Row, Col, InputNumber, Form } from "antd";
-import { useLocalStore, useObserver } from "mobx-react"
+import { PageHeader, Row, Col, InputNumber, Form, Table, Tabs } from "antd";
+import BSPricer from "./BSPricer";
 
-import { BS } from "../../pricingmodels/BlackScholes";
-import { exp } from "mathjs";
+const { TabPane } = Tabs;
 
-const StoreContext = React.createContext();
+const PricingTabs = () => (
+  <Tabs defaultActiveKey="1">
+    <TabPane tab="BS Pricer" key="1">
+      <BSPricer />
+    </TabPane>
+  </Tabs>
+);
 
-const StoreProvider = ({ children }) => {
-  const store = useLocalStore(() => ({
-    strike: 100.0,
-    underlying: 100.0,
-    volatility: 0.16,
-    time: 0.25,
-    rate: 0.00,
-    setStrike: x => (store.strike = x),
-    setUnderlying: x => (store.underlying = x),
-    setVolatility: x => (store.volatility = x),
-    setTime: x => (store.time = x),
-    setRate: x => (store.rate = x),
-    get price() {
-      console.log("Recompute")
-      return BS(store.underlying, store.strike, store.volatility, store.time, store.rate)
-    }
-  }))
-  return <StoreContext.Provider value={store}>{children}</StoreContext.Provider>
-}
-
-const Parameter = (props) => {
-  return (<Row>
-    <Col span={16}>
-      <Form.Item label={props.label} />
-    </Col>
-    <Col span={8}>
-      <InputNumber
-        step={props.step}
-        value={props.value}
-        onChange={(value) => {
-          console.log(`${props.label} changed to ${value}`);
-          props.setValue(value)
-        }}
-      />
-    </Col>
-  </Row>)
-}
-
-const Parameters = () => {
-  const store = React.useContext(StoreContext)
-  // const [strike, setStrike] = useState(props.strike);
-  // const underlyingPrice = props.underlying;
-  // const volatility = props.volatility;
-  // const riskFreeRate = props.riskFreeRate;
-  // const t = props.time;
-  // const recalculate = props.recalculate;
-
-  return useObserver(() => (
-    <>
-      <Parameter value={store.strike} step={0.25} setValue={store.setStrike} label={"Strike"} />
-      <Parameter value={store.underlying} step={0.25} setValue={store.setUnderlying} label={"Underlying Price"} />
-      <Parameter value={store.volatility} step={0.0001} setValue={store.setVolatility} label={"Volatility"} />
-      <Parameter value={store.rate} step={0.0001} setValue={store.setRate} label={"Rate"} />
-      <Parameter value={store.time} step={0.0001} setValue={store.setTime} label={"Time to Expiry"} />
-      <div>
-        {store.price}
-      </div>
-    </>
-  ))
-};
-
-
-const PricingPanel = (props) => {
+const PricingPanel = () => {
   return (
-    <StoreProvider>
+    <>
       <PageHeader
         className="site-page-header"
         title="Options Playground"
         subTitle="Playground to play with options pricing models"
       />
-      <Parameters />
-    </StoreProvider>
-  )
-}
+      <PricingTabs />
+    </>
+  );
+};
 
 export default PricingPanel;
-
-// class Options extends React.Component {
-//   render() {
-//     return (
-//       <>
-
-//         <Row>
-//           <Col span={12}>
-//             {/* <ParametersForm strike={100.0} underlying={100.0} /> */}
-//             <Parameters strike={100.0} recalculate={(p) => console.log(p)} />
-//           </Col>
-//           <Col></Col>
-//         </Row>
-//       </>
-//     );
-//   }
-// }
--- src/pricingmodels/Distributions.js
+++ src/pricingmodels/Distributions.js
@@ -1,27 +1,23 @@
-import { erf, sqrt } from 'mathjs'
-
+import { erf, sqrt } from "mathjs";
 
 const NormalDistribution = {
   cdf: (x, mean, sigma) => {
-    var z = (x - mean) / Math.sqrt(2 * sigma * sigma);
-    var t = 1 / (1 + 0.3275911 * Math.abs(z));
-    var a1 = 0.254829592;
-    var a2 = -0.284496736;
-    var a3 = 1.421413741;
-    var a4 = -1.453152027;
-    var a5 = 1.061405429;
-    var erf = 1 - (((((a5 * t + a4) * t) + a3) * t + a2) * t + a1) * t * Math.exp(-z * z);
-    var sign = 1;
-    if (z < 0) {
-      sign = -1;
-    }
-    return (1 / 2) * (1 + sign * erf);
+    return (1 - erf((mean - x) / (sqrt(2) * sigma))) / 2;
+  },
+  pdf: (x, mean, sigma) => {
+    const normalized = (x - mean) / sigma;
+    return (
+      Math.exp(-0.5 * normalized * normalized) / (sigma * sqrt(2 * Math.PI))
+    );
   },
 };
 
 const StandardNormalDistribution = {
   cdf: (x) => {
     return NormalDistribution.cdf(x, 0, 1.0);
+  },
+  pdf: (x) => {
+    return NormalDistribution.pdf(x, 0, 1.0);
   },
 };
 
