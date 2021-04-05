341a5d9503ae3995201a91db2d930cbf4ca1121f
Kapil Sharma <kapilsharma.frm@gmail.com>
2021-04-04 22:29:53 -0500

    Added initial snapshot of vgg transfer learning

---
+++ src/components/blog/contents/VGGDogBreedClassification.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {Collapse} from 'antd';
+import {Typography} from "antd";
+import {Alert} from 'antd';
+import {Link} from "react-router-dom";
+import {PythonSnippet} from "../snippets/PythonSnippet";
+import {Code, dracula} from "react-code-blocks";
+import {BashSnippet} from "../snippets/BashSnippet";
+import Image1 from "../../../../static/dog-breed-classification-1.png";
+import Image2 from "../../../../static/dog-breed-classification-2.svg";
+import Image3 from "../../../../static/impressed.gif";
+import Image4 from "../../../../static/dog-breed-classification-3.png";
+import Image5 from "../../../../static/dog-breed-classification-4.png";
+
+const {Title, Paragraph} = Typography;
+const {Panel} = Collapse;
+
+
+class VGGDogBreedClassification extends React.Component {
+    render() {
+        return (
+            <></>
+        )
+    }
+}
+
+export default VGGDogBreedClassification;
